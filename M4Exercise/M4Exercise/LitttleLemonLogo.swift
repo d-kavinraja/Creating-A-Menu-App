25f22017084af2e940ada6dfeaf5805a3a764457
Kavinraja.D <119875375+d-kavinraja@users.noreply.github.com>
2024-01-09 19:43:55 +0530

    Update LitttleLemonLogo.swift

--- M4Exercise/M4Exercise/LitttleLemonLogo.swift
+++ M4Exercise/M4Exercise/LitttleLemonLogo.swift
@@ -1,15 +1,11 @@
 import SwiftUI
-
 struct LittleLemonLogo: View {
     var body: some View {
         Image("littleLemon")
     }
 }
-
 struct LittleLemonLogo_Previews: PreviewProvider {
     static var previews: some View {
         LittleLemonLogo()
     }
 }
-
-
